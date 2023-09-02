167f88516cdc23f475a12e911aaeb34ab82696be
Tatsiana Shcherbo <scherbotaty@gmail.com>
2023-09-02 20:31:04 +0300

    Initial commit

---
+++ Sea-battle-project-visual/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Sea_battle_project_visual;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
