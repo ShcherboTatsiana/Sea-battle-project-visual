eac7d5c6e9bf8ff1597bebfc7f110c38052a8f2f
Татьяна Щербо <scherbotaty@gmail.com>
2023-09-12 22:57:03 +0300

    now ComputerFields are the same

--- Sea-battle-project-visual/ComputerField.axaml.cs
+++ Sea-battle-project-visual/ComputerField.axaml.cs
@@ -9,7 +9,7 @@ namespace sea_battle_project;
 
 [SuppressMessage("ReSharper", "CommentTypo")]
 public static class ComputerField
-{ 
+{
     // Заполняет поле кораблями
     public static void FillTheCompFieldWithShips()
     {
@@ -18,10 +18,16 @@ public static class ComputerField
         {
             numberOfShips[shipType]++;
             compShipsCounter++;
-            string shipName = shipType + numberOfShips[shipType].ToString();
+
+            string shipName = shipType.ToString() + numberOfShips[shipType].ToString();
+
             CreateShip(shipType);
-            MarkShipCells(shipType, shipName);
+            // Сортируем по возрастанию массив с координатами текущего корабля
             SelectionSortShipCoord(shipType);
+            // Присваиваем клеткам название текущего корабля
+            MarkShipCells(shipType, shipName);
+            // Помечаем клетки вокруг корабля Х-ми
+            // (= нельзя туда ставить часть другого корабля)
             MarkCellsAroundTheShip(shipType);
 
             // Проверяем, было ли расставлено на поле необходимое кол-во кораблей текущего типа
@@ -35,117 +41,115 @@ public static class ComputerField
     // Расставляет на поле корабль указанной длины
     private static void CreateShip(int shipLength)
     {
-        // Выбираем рандомно первую клетку
-        int vertIndex = randomizer.Next(1, 11);
-        int horizIndex = randomizer.Next(1, 11);
-        
+        /* Выбираем первую клетку корабля (её координаты) */
+
+        // Рандомно выбираем индекс клетки из списка свободных клеток
+        int index = randomizer.Next(0, freeCellsCounter);
+
+        // Получаем координаты
+        string[] cellCoord = freeCells[index].Split(',');
+        int vertIndex = Convert.ToInt32(cellCoord[0]);
+        int horizIndex = Convert.ToInt32(cellCoord[1]);
+
         // Если клетка попала в список неподходящих клеток, начинаем сначала
-        if (unfitCells.Contains(vertIndex + horizIndex.ToString()))
+        if (unfitCells.Contains(vertIndex.ToString() + horizIndex.ToString()))
         {
-            CreateShip(shipLength);
-            return;
-        }
-        
-        // Если клетка уже занята (там стоит корабль или же эта клетка находится возле корабля),
-        // начинаем сначала
-        if (compField[vertIndex, horizIndex] != "0")
-        {
-            unfitCells.Add(vertIndex + horizIndex.ToString());
             CreateShip(shipLength);
             return;
         }
 
-        // Помещаем координаты выбранной клетки в массив,
+        // Помещаем координаты выбранной клетки во временный массив,
         // который содержит кординаты клеток полученного корабля
         currShipCoordinates[0, 1] = vertIndex;
         currShipCoordinates[1, 1] = horizIndex;
 
-        // Алгоритм ниже рассчитан на корабли > 1
+        // Алгоритм ниже рассчитан на корабли длинной > 1
         if (shipLength == 1)
         {
             return;
         }
 
-        int cellNum = 2;        // Порядковый номер текущей подбираемой клетки
+        int cellNum = 2;               // Порядковый номер текущей подбираемой клетки
         bool isHorizontal = Convert.ToBoolean(randomizer.Next(0, 2));
         bool isToLeftOrTop = Convert.ToBoolean(randomizer.Next(0, 2));
         bool isChangeOfSideCanHelp = true;
         bool isChangeOfDirectionCanHelp = true;
-        int sideSign = 1;       // указывает сторону, в которой выбирается клетка: лево (верх) или право (низ)
+        int newShipSideSign = 1;       // указывает сторону, в которой выбирается клетка:
+                                       // 1 = лево (верх); -1 = право (низ)
 
         // Если было рандомно выбрано движение влево (вверх)
         if (isToLeftOrTop)
         {
-            sideSign *= -1;
+            newShipSideSign *= -1;
         }
-        
-        // Пока количество выбранных клеток != длинне создаваемого корабля
+
+        // Пока количество выбранных клеток != длине создаваемого корабля
         while (cellNum <= shipLength)
         {
             switch (isHorizontal)
             {
                 // Клетка по горизонтали пуста
-                case true when compField[vertIndex, horizIndex + sideSign] == "0":
-                {
-                    horizIndex += sideSign;
-                    currShipCoordinates[0, cellNum] = vertIndex;
-                    currShipCoordinates[1, cellNum] = horizIndex;
-                    break;
-                }
+                case true when compField[vertIndex, horizIndex + newShipSideSign] == "0":
+                    {
+                        horizIndex += newShipSideSign;
+                        currShipCoordinates[0, cellNum] = vertIndex;
+                        currShipCoordinates[1, cellNum] = horizIndex;
+                        break;
+                    }
                 // Достигли границы поля или неподходящей клетки
                 // Пытаемся выбрать клетку с другой стороны, если этого ещё не делали
                 case true when isChangeOfSideCanHelp:
-                {
-                    horizIndex = currShipCoordinates[1, 1];
-                    sideSign *= -1;
-                    isChangeOfSideCanHelp = false;
-                    continue;
-                }
+                    {
+                        horizIndex = currShipCoordinates[1, 1];
+                        newShipSideSign *= -1;
+                        isChangeOfSideCanHelp = false;
+                        continue;
+                    }
                 // Клетка по вертикали пуста
-                case false when compField[vertIndex + sideSign, horizIndex] == "0":
-                {
-                    vertIndex += sideSign;
-                    currShipCoordinates[0, cellNum] = vertIndex;
-                    currShipCoordinates[1, cellNum] = horizIndex;
-                    break;
-                }
+                case false when compField[vertIndex + newShipSideSign, horizIndex] == "0":
+                    {
+                        vertIndex += newShipSideSign;
+                        currShipCoordinates[0, cellNum] = vertIndex;
+                        currShipCoordinates[1, cellNum] = horizIndex;
+                        break;
+                    }
                 // Достигли границы поля или неподходящей клетки
                 // Пытаемся выбрать клетку с другой стороны, если этого ещё не делали 
                 case false when isChangeOfSideCanHelp:
-                {
-                    vertIndex = currShipCoordinates[0, 1];
-                    sideSign *= -1;
-                    isChangeOfSideCanHelp = false;
-                    continue;
-                }
+                    {
+                        vertIndex = currShipCoordinates[0, 1];
+                        newShipSideSign *= -1;
+                        isChangeOfSideCanHelp = false;
+                        continue;
+                    }
                 // Пытаемся расположить корабль в другом положении,
                 // если этого ещё не делали
                 case true or false when isChangeOfDirectionCanHelp:
-                {
-                    vertIndex = currShipCoordinates[0, 1];
-                    horizIndex = currShipCoordinates[1, 1];
-                    sideSign *= -1;
-                    isHorizontal = !isHorizontal;
-                    isChangeOfSideCanHelp = true;
-                    isChangeOfDirectionCanHelp = false;
-                    cellNum = 2;
-                    continue;
-                }
+                    {
+                        vertIndex = currShipCoordinates[0, 1];
+                        horizIndex = currShipCoordinates[1, 1];
+                        newShipSideSign *= -1;
+                        isHorizontal = !isHorizontal;
+                        isChangeOfSideCanHelp = true;
+                        isChangeOfDirectionCanHelp = false;
+                        cellNum = 2;
+                        continue;
+                    }
                 // Если корабль не удалось расположить ни в каком положении (вертикальном
                 // или горизонтальном), значит, первая клетка была выбрана неверно.
                 // Помещаем координаты этой клетки в список неподходящих клеток и
                 // начинаем процесс выбора первой клетки и создания корабля сначала
                 default:
-                {
-                    unfitCells.Add(currShipCoordinates[0, 1] + currShipCoordinates[1, 1].ToString());
-                    CreateShip(shipLength);
-                    return;
-                }
+                    {
+                        unfitCells.Add(currShipCoordinates[0, 1].ToString() + currShipCoordinates[1, 1].ToString());
+                        CreateShip(shipLength);
+                        return;
+                    }
             }
-            
+
             cellNum++;
         }
-        
+
         unfitCells.Clear();
     }
 
@@ -157,48 +161,26 @@ public static class ComputerField
         int cellNum = 1;
         while (cellNum <= shipLength)
         {
-            // Сохраняем координаты корабля в специальном массиве
-            compShipsCoordinates[compShipsCounter, 0, cellNum] = currShipCoordinates[0, cellNum];
-            compShipsCoordinates[compShipsCounter, 1, cellNum] = currShipCoordinates[1, cellNum];
             // Присваиваем клеткам с частью корабля идентификационный номер корабля
             int vertIndex = currShipCoordinates[0, cellNum];
             int horizIndex = currShipCoordinates[1, cellNum];
-            compField[vertIndex, horizIndex] = shipName;
+            compField[vertIndex, horizIndex] = shipName.ToString();
+
+            // Сохраняем координаты корабля в постоянном массиве,
+            // хранящем координаты клеток всех кораблей
+            compShipsCoordinates[compShipsCounter, 0, cellNum] = currShipCoordinates[0, cellNum];
+            compShipsCoordinates[compShipsCounter, 1, cellNum] = currShipCoordinates[1, cellNum];
+
+            // Убираем эту клетку из списка клеток,
+            // в которых можно расположить часть корабля / корабль
+            freeCells.Remove(vertIndex.ToString() + "," + horizIndex.ToString());
+            freeCellsCounter--;
+
             cellNum++;
         }
     }
-    
-    /* Сортирует по возрастанию массив currShipCoordinates, содержащий координаты
-     * клеток созданного перед вызовом этой фукнции корабля */
-    private static void SelectionSortShipCoord(int shipLength)
-    {
-        if (shipLength == 1)
-        {
-            return;
-        }
 
-        // Индекс ряда, который надо сортировать (в котором координаты отличаются)
-        int toSortRow = currShipCoordinates[0, 1] == currShipCoordinates[0, 2] ? 1 : 0;
-        for (int currIndex = 1; currIndex < shipLength; currIndex++)
-        {
-            int minElemIndex = currIndex;
-            int sepIndex = currIndex;     // Индекс разделителя, слева от которого: отсортированные элементы
-            while (sepIndex < shipLength)
-            {
-                sepIndex++;
-                if (currShipCoordinates[toSortRow, sepIndex] < 
-                    currShipCoordinates[toSortRow, minElemIndex])
-                {
-                    minElemIndex = sepIndex;
-                }
-            }
-
-            (currShipCoordinates[toSortRow, currIndex], currShipCoordinates[toSortRow, minElemIndex]) = 
-                (currShipCoordinates[toSortRow, minElemIndex], currShipCoordinates[toSortRow, currIndex]);
-        }
-    }
-
-    /* Заполняет клетки вокруг созданного корабля "1"-ми
+    /* Заполняет клетки вокруг созданного корабля "Х"-ми
      * (= в этих клетках нельзя располагать части другого корабля) */
     private static void MarkCellsAroundTheShip(int shipLength)
     {
@@ -211,9 +193,12 @@ public static class ComputerField
             while (horizIndex <= currShipCoordinates[1, shipLength] + 1)
             {
                 // Выбранная клетка не является частью корабля (=> принадлежит его окружению)
-                if (compField[vertIndex, horizIndex] is "0")
+                if (compField[vertIndex, horizIndex] == "0")
                 {
-                    compField[vertIndex, horizIndex] = "1";
+                    compField[vertIndex, horizIndex] = "Х";
+
+                    freeCells.Remove(vertIndex.ToString() + "," + horizIndex.ToString());
+                    freeCellsCounter--;
                 }
 
                 horizIndex++;
@@ -222,7 +207,7 @@ public static class ComputerField
             vertIndex++;
         }
     }
-    
+
     public static Random randomizer = new Random();
     public static string[,] compField =
     {
@@ -241,7 +226,7 @@ public static class ComputerField
     };
     private static int[] numberOfShips = { 0, 0, 0, 0, 0 };     // Число кораблей каждого типа 
     public static int compShipsCounter = 0;
-    
+
     /* Число клеток каждого из кораблей
      * Координата по горизонтали - длина корабля
      * Координата по вертикали - порядковый номер */
@@ -253,10 +238,31 @@ public static class ComputerField
         { 0, 1, 2, 0, 0 },
         { 0, 1, 0, 0, 0 }
     };
-    
+
     // Массив для хранения координат каждого из кораблей
     public static int[,,] compShipsCoordinates = new int[11, 2, 5];
-    
+
+    // Список незанятых клеток на поле
+    private static List<string> freeCells = new List<string>();
+    private static int freeCellsCounter = 100;
+
+    public static void FillTheFreeCellsList()
+    {
+        freeCells.AddRange(new[]
+        {
+            "1,1", "1,2", "1,3", "1,4", "1,5", "1,6", "1,7", "1,8", "1,9", "1,10",
+            "2,1", "2,2", "2,3", "2,4", "2,5", "2,6", "2,7", "2,8", "2,9", "2,10",
+            "3,1", "3,2", "3,3", "3,4", "3,5", "3,6", "3,7", "3,8", "3,9", "3,10",
+            "4,1", "4,2", "4,3", "4,4", "4,5", "4,6", "4,7", "4,8", "4,9", "4,10",
+            "5,1", "5,2", "5,3", "5,4", "5,5", "5,6", "5,7", "5,8", "5,9", "5,10",
+            "6,1", "6,2", "6,3", "6,4", "6,5", "6,6", "6,7", "6,8", "6,9", "6,10",
+            "7,1", "7,2", "7,3", "7,4", "7,5", "7,6", "7,7", "7,8", "7,9", "7,10",
+            "8,1", "8,2", "8,3", "8,4", "8,5", "8,6", "8,7", "8,8", "8,9", "8,10",
+            "9,1", "9,2", "9,3", "9,4", "9,5", "9,6", "9,7", "9,8", "9,9", "9,10",
+            "10,1", "10,2", "10,3", "10,4", "10,5", "10,6", "10,7", "10,8", "10,9", "10,10",
+        });
+    }
+
     // Список неподходящих на момент выполнения функции CreateShip() клеток
     private static List<string> unfitCells = new List<string>();
 }
